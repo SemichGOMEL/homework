c5abd60db1dfe0817543f82d364383919eef2907
Sema <semen.shilovski@yandex.ru>
2021-07-29 20:06:26 +0300

    Initial commit

---
+++ src/pr3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pr3 {
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        if(a>0){
+            a++;
+        } else if(a < 0){
+            a = a-2;
+        } else{
+            a = 10;
+        }
+        System.out.println(a);
+    }
+}
---
+++ src/pr4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pr4 {
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt(), b = inp.nextInt(), c = inp.nextInt(), pos = 0, neg = 0;
+        if(a > 0){
+            pos++;
+        } else if(a < 0){
+            neg++;
+        }
+        if(b > 0){
+            pos++;
+        } else if(b < 0){
+            neg++;
+        }
+        if(c > 0){
+            pos++;
+        } else if(c < 0){
+            neg++;
+        }
+        System.out.println("положительных - "+pos+", отрицательных - "+neg);
+    }
+}
